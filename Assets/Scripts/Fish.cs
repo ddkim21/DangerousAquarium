76cf1d09fe952eedde83e7546ae66720ccfe4c0f
ddkim21 <ddkim21@gmail.com>
2016-12-24 21:42:44 -0600

    Added Fish Movement
    Tested fish movement with walls.
    
    Fish are set to kinematic rigidbodies. In order to prevent them from
    moving off the boundaries of the playspace, I implemented a function
    that checks whether they are about to go off the playspace, and if they
    are, then they are to change direction to move away from the edge. At
    first, I implemented this function in the OnTriggerEnter2D, however, it
    seems as though it is better fit to be implemented in the Update()
    function. This is because the trigger function may not occur every
    frame, and this can cause the deter function to not be called, and thus
    fish can exit the camera frame. This has happened with 25 fish.
    
    Fish movement is determined randomly, and the time spent traveling in a
    certain direction is also determined randomly (or psuedorandomly).
    
    Tested on batches of fish, but have yet to test it on > 500 or so fish.

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour {
+
+	private int roamTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody2D>().freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	// Check if our fish is about to go out of bounds of the camera
+		if (OutOfBounds("Right")){
+			Deter("Right"); //If about to go out of bounds, slightly deter the direction
+		}							 // To prevent the fish from doing so
+		if (OutOfBounds("Left")){
+			Deter("Left");
+		}
+		if (OutOfBounds("Up")){
+			Deter("Up");
+		}
+		if (OutOfBounds("Down")){
+			Deter("Down");
+		}
+		// Otherwise, check if the roamtime of the fish is down, if so, then change direction
+		else if (roamTime <= 0){
+			SetTime();
+			SetDirection();
+		}
+		else { // Otherwise, count down on the roamtime for the given direction
+			roamTime--;
+		}
+	}
+
+	void SetTime() {
+		roamTime = Random.Range(30,360);
+	}
+
+	void SetDirection() {
+		Vector2 direction = new Vector2 (Random.Range(-10f,10f), Random.Range(-10f,10f));
+		this.GetComponent<Rigidbody2D>().velocity = direction;
+	}
+
+
+
+	void Deter(string side){
+		if (side == "Up"){
+			Vector2 deter = new Vector2 (this.GetComponent<Rigidbody2D>().velocity.x,
+															(-1) * Random.Range(0.1f,2f));
+			this.GetComponent<Rigidbody2D>().velocity = deter;
+			roamTime += 10;
+		}
+		else if (side == "Down"){
+			Vector2 deter = new Vector2 (this.GetComponent<Rigidbody2D>().velocity.x,
+															Random.Range(0.1f,2f));
+			this.GetComponent<Rigidbody2D>().velocity = deter;
+			roamTime += 10;
+		}
+		else if (side == "Left"){
+			Vector2 deter = new Vector2 (Random.Range(0.1f,2f), 
+															this.GetComponent<Rigidbody2D>().velocity.y);
+			this.GetComponent<Rigidbody2D>().velocity = deter;
+			roamTime += 10;
+		}
+		else {
+			Vector2 deter = new Vector2 ((-1) * Random.Range(0.1f,2f), 
+															this.GetComponent<Rigidbody2D>().velocity.y);
+			this.GetComponent<Rigidbody2D>().velocity = deter;
+			roamTime += 10;
+		}
+	}
+
+	bool OutOfBounds(string side){
+		if (side == "Right"){
+			return(this.transform.position.x + 2.25 > 50);
+		}
+		else if (side == "Left"){
+			return(this.transform.position.x - 2.25 < -50);
+		}
+		else if (side == "Up"){
+			return(this.transform.position.y + 1.65 > 28.125);
+		}
+		else {
+			return(this.transform.position.y - 1.65 < -28.125);
+		}
+	}
+
+
+}
